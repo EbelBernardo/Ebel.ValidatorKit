989d4e334e51ea2d27a0004e43ea7b570adc28ff
Bernardo Ebel <bernardoebel@gmail.com>
2026-02-02 15:29:05 -0300

    test(validation): adapt Validation tests to assert ValidationErrorCode

--- tests/Ebel.ValidationKit.Tests/Result/ValidationResultTests.cs
+++ tests/Ebel.ValidationKit.Tests/Result/ValidationResultTests.cs
@@ -12,16 +12,19 @@ public sealed class ValidationResultTests
 
         Assert.IsTrue(result.IsValid);
         Assert.AreEqual(string.Empty, result.Message);
+        Assert.AreEqual(ValidationErrorCode.None, result.Code);
     }
 
     [TestMethod]
     public void Fail_Should_Return_IsValid_False_And_Message()
     {
+        var errorCode = ValidationErrorCode.CpfInvalidCheckDigit;
         var errorMessage = "Error occurred";
 
-        var result = ValidationResult.Fail(errorMessage);
+        var result = ValidationResult.Fail(errorCode, errorMessage);
 
         Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(errorCode, result.Code);
         Assert.AreEqual(errorMessage, result.Message);
     }
 }
