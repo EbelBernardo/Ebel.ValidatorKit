bd421ea2154abdddf6fa72aba60babc031e4d332
Bernardo Ebel <bernardoebel@gmail.com>
2026-02-02 15:02:17 -0300

    refactor(validation): introduce ValidationErrorCode in ValidationResult

--- src/Ebel.ValidationKit/Results/ValidationResult.cs
+++ src/Ebel.ValidationKit/Results/ValidationResult.cs
@@ -3,11 +3,21 @@ namespace Ebel.ValidationKit.Results;
 public class ValidationResult()
 {
     public bool IsValid { get; set; }
+    public ValidationErrorCode Code { get; set; }
     public string Message { get; set; } = string.Empty;
 
     public static ValidationResult Success()
-        => new() { IsValid = true };
+        => new()
+        {
+            IsValid = true,
+            Code = ValidationErrorCode.None
+        };
 
-    public static ValidationResult Fail(string message)
-        => new() { IsValid = false, Message = message };
+    public static ValidationResult Fail(ValidationErrorCode code, string message)
+        => new()
+        {
+            IsValid = false,
+            Code = code,
+            Message = message
+        };
 }
